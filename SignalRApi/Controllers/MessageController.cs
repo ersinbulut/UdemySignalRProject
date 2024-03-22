8cb29e6f7fbeba86de31c6160b4c66f515e1d6ca
DEV <DEV@DESKTOP-8VP4IIT>
2024-03-22 16:07:50 +0300

    176. Mesaj Sınıfının Mimariye Eklenmesi

---
+++ SignalRApi/Controllers/MessageController.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SignalR.BusinessLayer.Abstract;
+using SignalR.DtoLayer.MessageDto;
+using SignalR.EntityLayer.Entities;
+
+namespace SignalRApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MessageController : ControllerBase
+    {
+        private readonly IMessageService _messageService;
+
+        public MessageController(IMessageService messageService)
+        {
+            _messageService = messageService;
+        }
+
+        [HttpGet]
+        public IActionResult MessageList()
+        {
+            var values = _messageService.TGetListAll();
+            return Ok(values);
+        }
+        [HttpPost]
+        public IActionResult CreateMessage(CreateMessageDto createMessageDto)
+        {
+            Message message = new Message()
+            {
+                NameSurname = createMessageDto.NameSurname,
+                Mail = createMessageDto.Mail,
+                Phone = createMessageDto.Phone,
+                Subject = createMessageDto.Subject,
+                MessageContent = createMessageDto.MessageContent,
+                MessageSendDate = DateTime.Now,
+                Status = false
+            };
+            _messageService.TAdd(message);
+            return Ok("Hakkında kısmı başarılı bir şekilde eklendi.");
+        }
+        [HttpDelete("{id}")]
+        public IActionResult DeleteMessage(int id)
+        {
+            var values = _messageService.TGetByID(id);
+            _messageService.TDelete(values);
+            return Ok("Mesaj silindi");
+        }
+        [HttpPut]
+        public IActionResult UpdateMessage(UpdateMessageDto updateMessageDto)
+        {
+            Message message = new Message()
+            {
+                MessageID= updateMessageDto.MessageID,
+                NameSurname = updateMessageDto.NameSurname,
+                Mail = updateMessageDto.Mail,
+                Phone = updateMessageDto.Phone,
+                Subject = updateMessageDto.Subject,
+                MessageContent = updateMessageDto.MessageContent,
+                MessageSendDate = updateMessageDto.MessageSendDate,
+                Status = false
+            };
+            _messageService.TUpdate(message);
+            return Ok("Mesaj güncellendi");
+        }
+        [HttpGet("{id}")]
+        public IActionResult GetAbout(int id)
+        {
+            var values = _messageService.TGetByID(id);
+            return Ok(values);
+        }
+    }
+}
